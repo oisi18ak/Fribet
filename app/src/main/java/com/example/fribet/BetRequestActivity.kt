51f2385b35ee3f3ce39c643c53bca422fa223184
Simon Oinonen <oisi18ak@student.ju.se>
2020-03-18 11:49:25 +0100

    Updated array adapter

--- app/src/main/java/com/example/fribet/BetRequestActivity.kt
+++ app/src/main/java/com/example/fribet/BetRequestActivity.kt
@@ -12,6 +12,7 @@ import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_bet_request.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.math.log
 
@@ -24,7 +25,12 @@ class BetRequestActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bet_request)
         val clicked = intent.getStringExtra("betId")
+        Firestore.instance.getBetById(clicked)
         Log.d("asd",clicked.toString())
+        val bet = BetRepository.instance.singleBet
+        Log.d("asd","${bet}")
+        //userText.text = bet[0].playerSending
+
 
     }
 }
